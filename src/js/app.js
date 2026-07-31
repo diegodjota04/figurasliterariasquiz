ec53e6f21dcb740bcd2dd82f86dd5ad05d54a74b
diegodjota04 <diego.djota@gmail.com>
2026-07-31 11:24:47 -0600

    corrección sobre error 404 json e icon

--- src/js/app.js
+++ src/js/app.js
@@ -145,7 +145,7 @@ document.addEventListener('DOMContentLoaded', () => {
       elements.btnStartGame.disabled = true;
     }
 
-    fetch('./data/versions.json')
+    fetch('.src/data/versions.json')
       .then(respuesta => {
         if (!respuesta.ok) {
           throw new Error('No se pudo acceder al archivo JSON.');
